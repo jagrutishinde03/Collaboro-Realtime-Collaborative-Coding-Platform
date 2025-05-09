772933fb26a8fb74b3ed8a6be476549eb5d625cc
jagrutishinde03 <jagruti03shinde@gmail.com>
2025-05-09 21:51:02 +0530

    Initial commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build by{" "}
+                <a
+                    href="https://github.com/sahilatahar"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Jagruti Shinde
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
